b475b2de4db4e2fc2713edb4452f282c4ef10265
DeepCoder <hunterdeepak122@gmail.com>
2024-01-01 20:41:51 +0530

    Get all Categories

--- OurWeb/Controllers/CategoryController.cs
+++ OurWeb/Controllers/CategoryController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OurWeb.Data;
+using OurWeb.Models;
 
 namespace OurWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            // var objCategoryList = _db.Categories.ToList(); // var and List<Category> works same so it's depend on you to use.
+            List<Category> objCategoryList=_db.Categories.ToList();
             return View();
         }
     }

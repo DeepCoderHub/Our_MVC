b003268e342c1701641a8c17d982dcef1b8eda7b
DeepCoder <hunterdeepak122@gmail.com>
2024-01-07 10:59:01 +0530

    Server Side Validation

--- OurWeb/Models/Category.cs
+++ OurWeb/Models/Category.cs
@@ -9,8 +9,10 @@ namespace OurWeb.Models
         public int Id  { get; set; }
         [Required]
         [DisplayName("Category Name")]
+        [MaxLength(30)]
         public string Name { get; set; }
         [DisplayName("Display Oreder")]
+        [Range(1,100,ErrorMessage ="Display Oredr must be between 1-100")]
         public int DisplayOrder { get; set; }
 
     }

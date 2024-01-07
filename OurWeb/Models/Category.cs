44630489f4566a948e54ebb9b161c9989dd1aa64
DeepCoder <hunterdeepak122@gmail.com>
2024-01-07 10:33:27 +0530

    Use Input Tag Helper

--- OurWeb/Models/Category.cs
+++ OurWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace OurWeb.Models
 {
@@ -7,8 +8,9 @@ namespace OurWeb.Models
         [Key]
         public int Id  { get; set; }
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
-        [Required]
+        [DisplayName("Display Oreder")]
         public int DisplayOrder { get; set; }
 
     }
